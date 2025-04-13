ebfe9fa18e5d72fdbe2497bfaa8e1452abc00123
nex7-7 <snehil.sinha784@gmail.com>
2025-04-13 15:39:28 +0530

    Import Cleanup

--- auth-server/src/main/java/com/example/auth_server/AuthServerApplication.java
+++ auth-server/src/main/java/com/example/auth_server/AuthServerApplication.java
@@ -3,7 +3,6 @@ package com.example.auth_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
